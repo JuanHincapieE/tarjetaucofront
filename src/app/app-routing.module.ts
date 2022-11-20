ec54b408fcebe5fdc9f7d85f8d45bab777757df6
Sebastian <juan.hincapie9893@uco.net.co>
2022-11-20 18:16:28 -0500

    primera carga front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardComponent } from './pages/card/card.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { StoresComponent } from './pages/stores/stores.component';
+import { UsComponent } from './pages/us/us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component:HomeComponent},
+  {path: "home", component:HomeComponent, data: {title: 'Inicio'}},
+  {path:"login", component:LoginComponent, data:{title: 'Iniciar sesión'}},
+  {path:"register", component:RegisterComponent, data:{title: 'Registrar'}},
+  {path:"stores", component:StoresComponent, data:{title: 'Tiendas'}},
+  {path:"card", component:CardComponent, data:{title: 'Tarjeta'}},
+  {path:"us", component:UsComponent, data:{title:'Nosotros'}}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { StoresComponent } from './pages/stores/stores.component';
+import { UsComponent } from './pages/us/us.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CardComponent } from './pages/card/card.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    StoresComponent,
+    UsComponent,
+    CardComponent,   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

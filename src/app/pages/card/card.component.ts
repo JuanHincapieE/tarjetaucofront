b0a60ae6a01f234f16c61e140812fdfc173d1ea5
Sebastian <juan.hincapie9893@uco.net.co>
2022-11-21 13:56:39 -0500

    Tarjeta digital primeros pasos

--- src/app/pages/card/card.component.ts
+++ src/app/pages/card/card.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+
+const card = document.querySelector('#card');
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
+
 export class CardComponent {
 
 }
+
+card?.addEventListener('click', () => {
+  card.classList.toggle('active');
+});
